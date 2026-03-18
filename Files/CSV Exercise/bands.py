6436e92278fff2873de2fd9b298c55ec6d450af3
B Manke <bmanke2@nait.ca>
2026-03-18 13:25:31 -0600

    Rename file and add dictwriter

--- Files/CSV Exercise/bands.py
+++ Files/CSV Exercise/bands.py
@@ -23,13 +23,33 @@ def sort_bands_by_stat(bands, column, reverse=True, top_n=3):
     sorted_bands = sorted(bands, key=lambda entry: entry[column], reverse=reverse)
     return sorted_bands[:top_n]
 
+def new_band(file_path, details):
+    with open(file_path, mode='a') as file:
+        headers = ['band_name', 'genre', 'crowd_score', 'judge_score', 'songs_performed', 'stage_energy']
+
+        writer = csv.DictWriter(file, headers)
+        writer.writerow(details)
 
 def main():
+
+    band = {
+        'band_name': 'Serpent',
+        'genre': 'Mathcore',
+        'crowd_score': 12,
+        'judge_score': 100,
+        'songs_performed': 1,
+        'stage_energy': 10
+    }
+
+    # "/CSV Exercise/"
     local_path = Path(__file__).parent
     file_name = 'data/bands.csv'
 
+    # "/CSV Exercise/data/bands.csv"
     file_path = local_path / file_name
     bands = read_bands(file_path)
+
+    new_band(file_path, band)
 
     columns = list(bands[0].keys())
     print('Available stats:')
@@ -37,7 +57,7 @@ def main():
         print(f"{i}. {col}")
 
     choice = int(input("Enter option: "))
-    result_count = int(input("How many results would you like?"))
+    result_count = int(input("How many results would you like? "))
     column = columns[choice]
     top_bands = sort_bands_by_stat(bands=bands, column=column, top_n=result_count)
     for i, band in enumerate(top_bands):
